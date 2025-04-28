6a1b95304e889c917448ab0ef80bed4b7f63018a
JordanBurga <jordanburga02@gmail.com>
2025-04-27 23:05:42 -0500

    Primer cambio

---
+++ src/jbtallergit/JB_ESTUDIANTE.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jbtallergit;
+
+/**
+ *
+ * @author User
+ */
+public class JB_ESTUDIANTE {
+    
+}
